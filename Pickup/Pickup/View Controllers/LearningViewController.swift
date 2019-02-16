b584c02126b069f779efe6c45f9ec25ee964845b
Keegan Davidson <davidsgk@miamioh.edu>
2019-02-16 16:56:08 -0500

    AudioKit enabled, frequency and amplitude measuring implemented

--- Pickup/Pickup/View Controllers/LearningViewController.swift
+++ Pickup/Pickup/View Controllers/LearningViewController.swift
@@ -7,24 +7,89 @@
 //
 
 import UIKit
+import AudioKit
+import AudioKitUI
 
 class LearningViewController: UIViewController {
-
+    
+    @IBOutlet private var frequencyLabel: UILabel!
+    @IBOutlet private var amplitudeLabel: UILabel!
+    @IBOutlet private var noteNameWithSharpsLabel: UILabel!
+    @IBOutlet private var noteNameWithFlatsLabel: UILabel!
+    @IBOutlet private var audioInputPlot: EZAudioPlot!
+    
+    var mic: AKMicrophone!
+    var tracker: AKFrequencyTracker!
+    var silence: AKBooster!
+    
+    let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
+    let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
+    let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
+    
+//    func setupPlot() {
+//        let plot = AKNodeOutputPlot(mic, frame: audioInputPlot.bounds)
+//        plot.plotType = .rolling
+//        plot.shouldFill = true
+//        plot.shouldMirror = true
+//        plot.color = UIColor.blue
+//        audioInputPlot.addSubview(plot)
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        AKSettings.audioInputEnabled = true
+        mic = AKMicrophone()
+        tracker = AKFrequencyTracker(mic)
+        silence = AKBooster(tracker, gain: 0)
+        print(mic.isStarted)
+        print(tracker.isStarted)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        AudioKit.output = silence
+        do {
+            try AudioKit.start()
+        } catch {
+            AKLog("AudioKit did not start!")
+        }
+//        setupPlot()
+        Timer.scheduledTimer(timeInterval: 0.5,
+                             target: self,
+                             selector: #selector(LearningViewController.updateUI),
+                             userInfo: nil,
+                             repeats: true)
     }
-    */
+    
+    @objc func updateUI() {
+        if tracker.amplitude > 0.1 {
+            frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
+            
+            var frequency = Float(tracker.frequency)
+            while frequency > Float(noteFrequencies[noteFrequencies.count - 1]) {
+                frequency /= 2.0
+            }
+            while frequency < Float(noteFrequencies[0]) {
+                frequency *= 2.0
+            }
+            
+            var minDistance: Float = 10_000.0
+            var index = 0
+            
+            for i in 0..<noteFrequencies.count {
+                let distance = fabsf(Float(noteFrequencies[i]) - frequency)
+                if distance < minDistance {
+                    index = i
+                    minDistance = distance
+                }
+            }
+            let octave = Int(log2f(Float(tracker.frequency) / frequency))
+            noteNameWithSharpsLabel.text = "\(noteNamesWithSharps[index])\(octave)"
+            noteNameWithFlatsLabel.text = "\(noteNamesWithFlats[index])\(octave)"
+        }
+        amplitudeLabel.text = String(format: "%0.3f", tracker.amplitude)
+    }
 
 }
